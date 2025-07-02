8101c2b8fcf90eeb76026931807a27b386015b8f
Facundo Pidal <facundopidalruizdiaz@gmail.com>
2025-07-02 11:59:17 -0300

    feat: enhance app-modal and confirm-modal components with improved styling and closing behavior

--- frontend/src/app/shared/components/app-modal.component.ts
+++ frontend/src/app/shared/components/app-modal.component.ts
@@ -8,11 +8,13 @@ import { Component, Output, EventEmitter } from '@angular/core';
       class="fixed inset-0 backdrop-blur-xs bg-zinc-800/70 bg-opacity-40 flex items-center justify-center z-50"
     >
       <div
-        class="bg-white rounded shadow-xl border-[1px] border-zinc-800 p-6 w-[80lvw] md:w-xl relative"
+        class="bg-white rounded shadow-xl border-[1px] border-zinc-800 py-10 px-6 w-[80lvw] md:w-xl relative 
+        starting:scale-0 transition-transform duration-300 ease-in-out "
+        [class]="{ 'scale-100': !isClosing, 'scale-0': isClosing }"
       >
         <button
-          (click)="closeEvent.emit()"
-          class="absolute top-2 right-2 text-zinc-500 hover:text-black"
+          (click)="close()"
+          class="absolute top-2 right-2 text-zinc-500 hover:text-black text-2xl cursor-pointer"
         >
           ✕
         </button>
@@ -24,4 +26,13 @@ import { Component, Output, EventEmitter } from '@angular/core';
 })
 export class AppModalComponent {
   @Output() closeEvent = new EventEmitter<void>();
+  isClosing = false;
+
+  close() {
+    this.isClosing = true;
+    setTimeout(() => {
+      this.closeEvent.emit();
+      this.isClosing = false;
+    }, 300);
+  }
 }
--- frontend/src/app/shared/components/confirm-modal.component.ts
+++ frontend/src/app/shared/components/confirm-modal.component.ts
@@ -8,20 +8,20 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
       class="fixed inset-0 backdrop-blur-xs bg-zinc-800/70 bg-opacity-40 flex items-center justify-center z-50"
     >
       <div
-        class="bg-white rounded shadow-xl border-[1px] border-zinc-800 p-6 min-w-[300px]"
+        class="bg-white rounded shadow-xl border border-zinc-800 p-6 min-w-[300px]"
       >
         <h2 class="text-lg font-semibold mb-4">{{ title }}</h2>
         <p class="mb-6">{{ message }}</p>
-        <div class="flex justify-end gap-2">
+        <div class="flex justify-center gap-2">
           <button
             (click)="cancelClick.emit()"
-            class="px-4 py-2 rounded bg-zinc-200 hover:bg-zinc-300"
+            class="px-4 py-2 rounded bg-zinc-200 hover:bg-zinc-300 cursor-pointer"
           >
             Cancelar
           </button>
           <button
             (click)="confirm.emit()"
-            class="px-4 py-2 rounded bg-red-500 text-white hover:bg-red-600"
+            class="px-4 py-2 rounded bg-red-500 text-white hover:bg-red-600 cursor-pointer"
           >
             Confirmar
           </button>
